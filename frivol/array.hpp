34c9a98a7b96298035e1f1b5921f69624b1506b3
Topi Talvitie <topi.talvitie@iki.fi>
2013-05-07 20:00:33 +0300

    Added doxygen config and fixed doxygen documentation of Array.

--- frivol/array.hpp
+++ frivol/array.hpp
@@ -16,10 +16,10 @@ public:
 	/// @param size The size of the array.
 	Array(Idx size);
 	
-	/// Returns reference to an element in the array.
-	/// @param index The zero-based index in the array.
-	/// @throws std::out_of_range if FRIVOL_ARRAY_BOUNDS_CHECKING is defined and 'index' overflows.
 	/// @{
+	/// Returns reference to an element in the array.
+	/// @param index The zero-based index of the element.
+	/// @throws std::out_of_range if FRIVOL_ARRAY_BOUNDS_CHECKING is defined and 'index' overflows.
 	const T& operator[](Idx index) const;
 	T& operator[](Idx index);
 	/// @}
