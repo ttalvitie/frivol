e06883695e7ebccc7d9fadadcd0b2da2a3bf5752
Topi Talvitie <topi.talvitie@iki.fi>
2013-05-15 03:24:28 +0300

    Added tests for search tree.

---
+++ test/search_tree.cpp
@@ -0,0 +1,73 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
+#include "frivol/search_tree_concept.hpp"
+
+using namespace frivol;
+
+BOOST_AUTO_TEST_SUITE(search_tree)
+
+typedef boost::mpl::list<DummySearchTree<int>> SearchTreeTypes;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(implements_concept, SearchTree, SearchTreeTypes) {
+	BOOST_CONCEPT_ASSERT((SearchTreeConcept<SearchTree, int>));
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(basic_insert_erase_empty, SearchTree, SearchTreeTypes) {
+	SearchTree t;
+	BOOST_CHECK(t.empty());
+	t.insert(t.end(), 51);
+	t.insert(t.end(), 1);
+	BOOST_CHECK(!t.empty());
+	t.erase(t.begin());
+	BOOST_CHECK(!t.empty());
+	t.erase(t.begin());
+	BOOST_CHECK(t.empty());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(iteration_works, SearchTree, SearchTreeTypes) {
+	SearchTree t;
+	typename SearchTree::Iterator iter = t.end();
+	t.insert(iter, 5);
+	t.insert(iter, 8);
+	--iter;
+	t.insert(iter, 3);
+	--iter;
+	--iter;
+	BOOST_CHECK(iter == t.begin());
+	
+	BOOST_CHECK_EQUAL(*iter++, 5);
+	BOOST_CHECK_EQUAL(*iter++, 3);
+	BOOST_CHECK_EQUAL(*iter++, 8);
+	
+	BOOST_CHECK(iter == t.end());
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(search_positive_works, SearchTree, SearchTreeTypes) {
+	SearchTree t;
+	typedef typename SearchTree::Iterator Iterator;
+	t.insert(t.end(), 1);
+	t.insert(t.end(), 4);
+	t.insert(t.end(), 6);
+	t.insert(t.end(), 7);
+	t.insert(t.end(), 9);
+	Iterator result = t.search([](Iterator iter) { return 6 - *iter; });
+	
+	BOOST_CHECK(result != t.end());
+	BOOST_CHECK_EQUAL(*result, 6);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(search_negative_works, SearchTree, SearchTreeTypes) {
+	SearchTree t;
+	typedef typename SearchTree::Iterator Iterator;
+	t.insert(t.end(), 1);
+	t.insert(t.end(), 4);
+	t.insert(t.end(), 6);
+	t.insert(t.end(), 7);
+	t.insert(t.end(), 9);
+	Iterator result = t.search([](Iterator iter) { return 8 - *iter; });
+	
+	BOOST_CHECK(result == t.end());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
