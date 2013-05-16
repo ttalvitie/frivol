d87b51fd975724b63edc9b387587e91c99b65352
Topi Talvitie <topi.talvitie@iki.fi>
2013-05-17 00:52:43 +0300

    Added more tests to fortune_algorithm.

--- test/fortune_algorithm.cpp
+++ test/fortune_algorithm.cpp
@@ -35,4 +35,26 @@ BOOST_AUTO_TEST_CASE(sites_in_triangle_one_voronoi_vertex) {
 	BOOST_CHECK_EQUAL(1, algo.getVoronoiVertexCount());
 }
 
+BOOST_AUTO_TEST_CASE(diamond_two_voronoi_vertices) {
+	Array<Point<>> sites(4);
+	sites[0] = Point<>(-2, 0);
+	sites[1] = Point<>(2, 0);
+	sites[2] = Point<>(0, -1);
+	sites[3] = Point<>(0, 1);
+	fortune::Algorithm<> algo(sites);
+	algo.finish();
+	BOOST_CHECK_EQUAL(2, algo.getVoronoiVertexCount());
+}
+
+BOOST_AUTO_TEST_CASE(four_sites_three_voronoi_vertices) {
+	Array<Point<>> sites(4);
+	sites[0] = Point<>(-1, 0);
+	sites[1] = Point<>(1, 0);
+	sites[2] = Point<>(0, 1);
+	sites[3] = Point<>(0, 2);
+	fortune::Algorithm<> algo(sites);
+	algo.finish();
+	BOOST_CHECK_EQUAL(3, algo.getVoronoiVertexCount());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
